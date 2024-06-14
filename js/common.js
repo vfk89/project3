94a379f59e23c049f34c824ffa40b18b59dbc69a
VV <you@example.com>
2024-06-14 16:32:07 +0300

    Initial commit

---
+++ js/common.js
@@ -0,0 +1,30 @@
+$(function() {
+
+	new WOW().init();
+
+	$(".hamburger").click(function(){
+		$(this).toggleClass("is-active");
+
+		if($(this).hasClass('is-active')){
+			$('.mnu_top').slideDown(300);
+		}else{
+			$('.mnu_top').slideUp(300);
+		}
+	});
+
+
+	/* reveiws tabs */	
+	$('.nav_rev > div').click(function(){
+		const revID = $(this).data('revid');
+
+		$('.nav_rev > div').not(this).removeClass('active');
+		$(this).addClass('active');
+
+		$('.item_rev').not(revID).removeClass('active');
+		$(revID).addClass('active');
+
+	});
+	/* reveiws tabs */
+
+});
+
